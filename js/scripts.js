b82cc311ab5321c9d682102a7be5bdfd43ccd784
Hann <holyrebas@gmail.com>
2021-05-04 15:21:57 +0900

    map test 5

--- js/scripts.js
+++ js/scripts.js
@@ -137,16 +137,6 @@ $(document).ready(function () {
 
     });
 
-    $(function () {
-        var container = document.getElementById('map-canvas'); //지도를 담을 영역의 DOM 레퍼런스
-        var options = { //지도를 생성할 때 필요한 기본 옵션
-            center: new kakao.maps.LatLng(37.521813415351026, 126.91914485010443), //지도의 중심좌표.
-            level: 3 //지도의 레벨(확대, 축소 정도)
-        };
-    
-        var map = new kakao.maps.Map(container, options);
-    });
-
     /********************** Embed youtube video *********************/
     $('.player').YTPlayer();
 
@@ -231,17 +221,6 @@ function initMap() {
         position: location,
         map: map
     });
-}
-
-// Kakao Map
-function initKakaoMap() {
-    var container = document.getElementById('map-canvas'); //지도를 담을 영역의 DOM 레퍼런스
-    var options = { //지도를 생성할 때 필요한 기본 옵션
-        center: new kakao.maps.LatLng(37.521813415351026, 126.91914485010443), //지도의 중심좌표.
-        level: 3 //지도의 레벨(확대, 축소 정도)
-    };
-
-    var map = new kakao.maps.Map(container, options);
 }
 
 // alert_markup
